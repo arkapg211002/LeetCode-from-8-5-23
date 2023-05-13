8e27a4f612a6b45c0cb8bb0fac3c453a3034f9e9
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-05-13 08:41:53 +0530

    Time: 21 ms (29.20%) | Memory: 54.8 MB (22.12%) - LeetSync

--- count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -3,16 +3,16 @@ class Solution {
     public int countGoodStrings(int low, int high, int zero, int one) {
         int dp[]=new int[high+1];
         Arrays.fill(dp,-1);
-        return dfs(low,high,zero,one,0,dp);
+        return f(0,low,high,zero,one,dp);
     }
-    public int dfs(int low,int high,int zero, int one,int ind,int dp[])
+    public int f(int ind,int low,int high,int zero, int one,int dp[])
     {
         if(ind>high) return 0;
         if(dp[ind]!= -1) return dp[ind];
         int count = 0;
         if(ind>=low && ind<=high) count=(count+1)%MOD;
-        count=(count+dfs(low,high,zero,one,ind+zero,dp))%MOD;
-        count=(count+dfs(low,high,zero,one,ind+one,dp))%MOD;
-        return dp[ind]=count;
+        count=(count + f(ind+zero,low,high,zero,one,dp))%MOD;
+        count=(count + f(ind+one,low,high,zero,one,dp))%MOD;
+        return dp[ind]=count%MOD;
     }
 }
