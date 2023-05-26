4d96ca31c543f81a7e85d83ff07041536afe8a8a
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-05-26 13:52:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (98.35%) - LeetSync

---
+++ rotate-list/rotate-list.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if( k == 0 || head ==null || head.next == null) return head;
+
+        int len=1;
+        ListNode cur=head;
+        //count and travel to the last node;
+        while(cur.next != null){
+            len++;
+            cur=cur.next;
+        }
+        //when k=len we get the same linked list
+        k = len - (k%len);
+        //make a loop by attaching last to first
+        cur.next = head;
+
+        while(k > 0){
+            cur=cur.next;
+            k--;
+        } 
+        //get the head and break the link
+        head = cur.next;
+        cur.next = null;
+
+        return head;
+
+    }
+}
