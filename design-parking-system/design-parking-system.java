e7ca264aaa77b589f74f2dd09932376750f99591
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-05-29 07:28:30 +0530

    Time: 9 ms (56.91%) | Memory: 44.7 MB (6.49%) - LeetSync

--- design-parking-system/design-parking-system.java
+++ design-parking-system/design-parking-system.java
@@ -1,32 +1,27 @@
 class ParkingSystem {
-    private int bigCount; //keeps track of big cars
-    private int mediumCount; //keeps track of medium cars
-    private int smallCount; //keeps track of small cars
-    public ParkingSystem(int big, int medium, int small) {//constructor
-        //setting values
+    public int bigCount, mediumCount, smallCount; 
+    public ParkingSystem(int big, int medium, int small) 
+    {
         this.bigCount = big;
         this.mediumCount = medium;
         this.smallCount = small;
     }
-    
-    public boolean addCar(int carType) {
-        if(carType == 1){ //checks for car type
-            if(bigCount==0) return false; //if maximum capacity reached, returns false
-            else {
-                bigCount--; //otherwise it decreases available space by 1 and returns true
-            }
+    public boolean addCar(int carType)
+    {
+        if(carType == 1)
+        { 
+            if(bigCount==0) return false; 
+            else bigCount--; 
         }
-        else if(carType == 2){
+        else if(carType == 2)
+        {
             if(mediumCount==0) return false;
-            else {
-                mediumCount--;
-            }
+            else mediumCount--;
         }
-        else if(carType == 3){
+        else if(carType == 3)
+        {
             if(smallCount==0) return false;
-            else {
-                smallCount--;
-        }
+            else smallCount--;
         }
         return true;
     }
