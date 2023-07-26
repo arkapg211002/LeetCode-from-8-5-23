3c236deb6785431fcb2465440a14c8f62ae83de0
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-07-26 19:49:28 +0530

    Time: 147 ms (83.05%), Space: 58.9 MB (68.17%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        if (hour<=dist.length-1) return -1;
+        int lo=1;
+        int hi=Integer.MAX_VALUE;
+        while (lo < hi) 
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (check(dist, hour, mid)) hi = mid;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+    public boolean check(int[] dist, double hour, int mid) 
+    {
+        double sum=0;
+        for (int i=0;i<dist.length;i++) 
+        {
+            if (i == dist.length - 1) sum += (double) dist[i] / mid;
+            else sum += dist[i] / mid + (dist[i] % mid == 0 ? 0 : 1);
+        }
+        return (sum<=hour);
+    }
+}
