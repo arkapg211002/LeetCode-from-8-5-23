7c37a5902127b01703896664bbf9ea296bbf9b70
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-08-14 20:00:42 +0530

    Time: 1 ms (80.90%), Space: 40.2 MB (93.97%) - LeetHub

---
+++ 0388-longest-absolute-file-path/0388-longest-absolute-file-path.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int lengthLongestPath(String input) {
+        var stack = new ArrayDeque<Integer>();
+        int max = 0;
+        String[] lines = input.split("\n");
+        for(var line: lines) {
+            int tabs = countTabs(line);
+            while(tabs < stack.size()) {
+                stack.pop();
+            }
+            int current = stack.isEmpty() ? 0: stack.peek();
+            int nameLength = line.length() - tabs;
+            if(isFilename(line)) {
+                max = Math.max(max, current + nameLength);
+            } else {
+                stack.push(current + nameLength + 1);
+            }
+        }
+        return max;
+    }
+    
+    private int countTabs(String s) {
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != '\t') return i;
+        }
+        return 0;
+    }
+    
+    private boolean isFilename(String s) {
+        return s.lastIndexOf(".") != -1;
+    }
+    
+}
