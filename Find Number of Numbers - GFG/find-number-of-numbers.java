f13cd607e46f62bd9a8fc4f09a23d3990eaae4c6
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-08-31 10:05:28 +0530

    Added solution - LeetHub

---
+++ Find Number of Numbers - GFG/find-number-of-numbers.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.util.*;
+class Numbers{
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t-->0)
+	{
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i = 0; i < n; i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int k=sc.nextInt();
+		GfG g=new GfG();
+		System.out.println(g.num(a,n,k));
+	}
+}
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG
+{
+          public static int num(int a[], int n, int k)
+            {
+                int c = 0;
+                 //Your code here
+                 for(int i = 0 ; i < n ; i++)
+                 {
+                    while(a[i] > 0)
+                    {
+                         int h = a[i] % 10;
+                         if(h == k)
+                         {
+                         c++;
+                         }
+                         a[i] /= 10;
+                     
+                 }
+                 }
+                 return c;
+            }
+}
