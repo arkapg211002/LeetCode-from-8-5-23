0d69f59159807f3ad4138a6dba23a79eced2e65c
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-09-09 20:47:46 +0530

    Added solution - LeetHub

---
+++ Sum of Consecutives - GFG/sum-of-consecutives.java
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long n = Long.parseLong(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.canBeSumofConsec(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static long canBeSumofConsec(long n) { 
+        // code here
+        if(n==1L) return 0L;
+        else if((n & (n-1))==0L) return 0L;
+        else return 1L;
+    }
+}
