117010bed8a283484c5b55fb55d3c494a6c50a0a
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-11-19 10:05:20 +0530

    Time: 34 ms (81.58%), Space: 55.4 MB (25.00%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.java
@@ -7,14 +7,3 @@ class Solution {
         return res;
     }
 }
-
-/*
-public int reductionOperations(int[] n) {
-    int res = 0, sz = n.length;
-    Arrays.sort(n);
-    for (int j = sz - 1; j > 0; --j)
-        if (n[j - 1] != n[j])
-            res += sz - j;
-    return res;
-}
-*/
