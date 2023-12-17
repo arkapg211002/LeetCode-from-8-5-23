f09d1fd33941862a93d4fdcd9341b2b1721255ef
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2023-12-17 11:51:34 +0530

    Time: 190 ms (82.49%), Space: 75.6 MB (20.90%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.java
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.java
@@ -1,51 +1,51 @@
-class FoodRatings 
-{
-    public class Pair
-    {
-        String f;
-        int i;
-        int rating;
-        public Pair(String food, int index, int ratings)
-        {
-            f = food;
-            i = index;
-            rating = ratings;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class FoodRatings {
+
+    public class FoodPair {
+        String foodName;
+        int foodIndex;
+        int foodRating;
+
+        public FoodPair(String food, int index, int rating) {
+            foodName = food;
+            foodIndex = index;
+            foodRating = rating;
         }
     }
-    HashMap<String, Integer> map = new HashMap<>();
-    HashMap<String, PriorityQueue<Pair>> pqMap = new HashMap<>();
-    int[] rates;
-    String[] f;
-    String[] cus;
-    public FoodRatings(String[] foods, String[] cuisines, int[] ratings)
-    {
-        for(int i = 0; i < foods.length; i++)
-        {
-            pqMap.putIfAbsent(cuisines[i], new PriorityQueue<Pair>((a, b) -> b.rating == a.rating ? a.f.compareTo(b.f) : b.rating - a.rating));
-            pqMap.get(cuisines[i]).offer(new Pair(foods[i], i, ratings[i]));
-            map.put(foods[i], i);
-            rates = ratings;
-            f = foods;
-            cus = cuisines;
+
+    private HashMap<String, Integer> foodIndexMap = new HashMap<>();
+    private HashMap<String, PriorityQueue<FoodPair>> cuisinePriorityQueueMap = new HashMap<>();
+    private int[] foodRatings;
+    private String[] foodNames;
+    private String[] cuisines;
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        for (int i = 0; i < foods.length; i++) {
+            cuisinePriorityQueueMap.putIfAbsent(cuisines[i], new PriorityQueue<>(Comparator.comparing(
+                    (FoodPair a) -> a.foodRating).reversed().thenComparing(a -> a.foodName)));
+            cuisinePriorityQueueMap.get(cuisines[i]).offer(new FoodPair(foods[i], i, ratings[i]));
+            foodIndexMap.put(foods[i], i);
+            foodRatings = ratings;
+            foodNames = foods;
+            this.cuisines = cuisines;
         }
     }
-    
-    public void changeRating(String food, int newRating) 
-    {
-        int index = map.get(food);
-        rates[index] = newRating;
-        String c = cus[index];
-        pqMap.get(c).offer(new Pair(f[index], index, newRating));
+
+    public void changeRating(String food, int newRating) {
+        int index = foodIndexMap.get(food);
+        foodRatings[index] = newRating;
+        String cuisine = cuisines[index];
+        cuisinePriorityQueueMap.get(cuisine).offer(new FoodPair(foodNames[index], index, newRating));
     }
-    
-    public String highestRated(String cuisine) 
-    {
-        PriorityQueue<Pair> pq = pqMap.get(cuisine);
-        while(rates[pq.peek().i] != pq.peek().rating)
-        {
-            pq.poll();
-        }    
-        return pq.peek().f;
+
+    public String highestRated(String cuisine) {
+        PriorityQueue<FoodPair> cuisinePriorityQueue = cuisinePriorityQueueMap.get(cuisine);
+        while (foodRatings[cuisinePriorityQueue.peek().foodIndex] != cuisinePriorityQueue.peek().foodRating) {
+            cuisinePriorityQueue.poll();
+        }
+        return cuisinePriorityQueue.peek().foodName;
     }
 }
 
