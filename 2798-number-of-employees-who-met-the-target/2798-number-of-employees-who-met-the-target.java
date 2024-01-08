91f96716fc97b6f559be7dd31e542635dc0ff30c
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-01-08 10:47:09 +0530

    Time: 1 ms (47.03%), Space: 42.7 MB (6.46%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        Arrays.sort(hours);
+        int l=0,r=hours.length-1,id=-1;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(hours[mid]>=target)
+            {
+                id=mid;
+                r=mid-1;
+            }
+            else
+                l=mid+1;
+        }
+        //System.out.println(id);
+        if(id!=-1) return hours.length-id;
+        else return 0;
+    }
+}
