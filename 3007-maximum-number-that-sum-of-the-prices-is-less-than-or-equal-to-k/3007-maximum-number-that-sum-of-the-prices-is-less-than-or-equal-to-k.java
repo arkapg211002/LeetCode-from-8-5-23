4c657530f6d60a5d5418e61637b5463d2430323e
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-01-14 10:32:16 +0530

    Time: 11 ms (33.33%), Space: 40.9 MB (33.33%) - LeetHub

--- 3007-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/3007-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.java
+++ 3007-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k/3007-maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.java
@@ -1,43 +1,34 @@
 class Solution {
-    // Check for the current binary search query
-    private boolean valid(long mid, long x, long k) {
-        // Number of 1's in required bit positions up to the number mid
-        long sum = 0;
-        for (long i = x - 1; i <= 60; i += x) {
-            // Calculate the dividend and pattern size
-            long dividend = mid + 1;
-            long patsize = (1L << (i + 1));
-
-            // Calculate the sum based on pattern size and dividend
-            sum += patsize / 2 * (dividend / patsize);
-
-            // Handle the remainder
-            long rem = dividend % patsize;
-            if (rem <= patsize / 2) continue;
-            rem %= (patsize / 2);
-            sum += rem;
-        }
-        // Return true if the sum is less than or equal to k
-        return sum <= k;
-    }
-
-    // Perform binary search to find the maximum number
     public long findMaximumNumber(long k, int x) {
-        // Initialize the binary search range
-        long low = 1, high = (long) 1e15, mid;
-
-        // Binary search loop
-        while (low <= high) {
-            mid = low + (high - low) / 2;
-
-            // Check if the current mid is valid, update the search range accordingly
-            if (valid(mid, x, k)) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        long left=1L,right=(long)1e15,ans=0L;
+        while(left<=right)
+        {
+            long mid=left+(right-left)/2;
+            if(valid(mid,x,k))
+            {
+                ans=(long)Math.max(ans,mid);
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
             }
         }
-        // Return the maximum number found
-        return high;
+        return ans;
+    }
+    public boolean valid(long mid,int x,long k)
+    {
+        long sum=0L;
+        for(int i=1;i<64;i++)
+        {
+            if(i%x!=0) continue;
+            long pow=(long)Math.pow(2,i);
+            long rem=mid%pow;
+            long pow2=pow/2;
+            sum+=(mid/pow)*pow2;
+            if(rem>=pow2) sum+=rem+1-pow2;
+            if(sum>k) return false;
+        }
+        return true;
     }
 }
