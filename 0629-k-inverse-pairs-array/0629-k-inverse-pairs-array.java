a09bb44f72544f202d3d6e2e2adc7a13dec9c8a7
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-01-27 10:08:19 +0530

    Time: 1201 ms (21.01%), Space: 56.3 MB (15.97%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    int MOD = 1_000_000_007;
+    public int kInversePairs(int n, int k) {
+        int[][] dp = new int[1001][1001];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                for (int x = 0; x <= Math.min(j, i - 1); x++) {
+                    if (j - x >= 0) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % MOD;
+                    }
+                }
+            }
+        }
+        return dp[n][k];
+    }
+}
