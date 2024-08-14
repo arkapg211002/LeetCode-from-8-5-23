8e7fd5a687d80448eaf608511a640ce02e767437
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2024-08-14 08:39:24 +0530

    719. Find K-th Smallest Pair Distance

---
+++ 719. Find K-th Smallest Pair Distance/Solution.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/?envType=daily-question&envId=2024-08-14
+
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        Arrays.sort(nums);
+        int low = 0, high = nums[nums.length - 1] - nums[0];
+        
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (countPairs(nums, mid) >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
+
+    private int countPairs(int[] nums, int mid) {
+        int count = 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (j < nums.length && nums[j] - nums[i] <= mid) {
+                j++;
+            }
+            count += j - i - 1;
+        }
+        return count;
+    }
+}
